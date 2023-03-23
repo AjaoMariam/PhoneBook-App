70a1db3538c94bbb23647ef42fb2549da3c7fcf3
Ajao Mariam O <105339552+AjaoMariam@users.noreply.github.com>
2023-03-23 12:52:10 +0100

    Add files via upload

---
+++ PhoneBookApp/PhoneBookApp/PhoneBook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp
+{
+    public class PhoneBook
+    {
+        private List<Contact> _contact { get; set; } = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            _contact.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contact.FirstOrDefault();
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+                //Console.WriteLine(contact);
+            }
+        }
+
+        //public void DisplayContactDetails(Contact contact)
+        //{
+        //    Console.WriteLine($"Contact{contact.Name}," + $"{contact.Number}");
+        //}
+
+        public void DisplayAllContact()
+        {
+            foreach (var contact in _contact)
+            {
+                DisplayContactDetails(contact);
+            }
+
+        }
+        public void DisplayContactsDetails(List<Contact> contacts) 
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name}"+ $" Number: {contact.Number} \n");
+        }
+        public void DisplayMatchingContact(string searchPhrase)
+        {
+            var matchingContacts = _contact.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContactsDetails(matchingContacts);
+        }
+    }
+}
---
+++ PhoneBookApp/PhoneBookApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the console PhoneBook App");
+            Console.WriteLine("Select Operation");
+            Console.WriteLine("1 Add Contact");
+            Console.WriteLine("2 Display Contact By Number");
+            Console.WriteLine("3 View All Contacts");
+            Console.WriteLine("4 Search for contact for a given Name");
+            Console.WriteLine("Press x to Exit");
+
+            var phoneBook = new PhoneBook();
+
+            while(true)
+            {
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Contact Name:");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Contact Number:");
+                        var number = Console.ReadLine();
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+                        Console.WriteLine("\n\nSelect Operation");
+                        break;
+                    case "2":
+                        Console.WriteLine("Contact Number to search");
+                        var searchNumber = Console.ReadLine();
+                        phoneBook.DisplayContact(searchNumber);
+                        Console.WriteLine("\n\nSelect Operation");
+
+                         break;
+                    case "3":
+                        phoneBook.DisplayAllContact();
+                        Console.WriteLine("\n\nSelect Operation");
+                        break;
+                    case "4":
+                        Console.WriteLine("Search Name by Name Phrase");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContact(searchPhrase);
+                        Console.WriteLine("\n\nSelect Operation");
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("Please Select Valid Operation");
+                        break;
+                }
+
+            }
+
+
+
+            
+        }
+    }
+}
